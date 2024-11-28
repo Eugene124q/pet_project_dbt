a2adff03a9f13a70f32fea07be6c79feec5ab2ac
evgenij dmitriev <evgenij@3536425-tk81101.twc1.net>
2024-11-28 09:25:05 +0300

    создание albums_metrics_view sources.yml

---
+++ models/my_sql_models/albums_metrics_view.sql
@@ -0,0 +1,11 @@
+WITH ranked_albums AS (
+         SELECT albums_metrics.album_id,
+            first_value(albums_metrics.likes_count) OVER (PARTITION BY albums_metrics.album_id, (date(albums_metrics.dt)) ORDER BY albums_metrics.dt DESC) AS likes_count,
+            date(albums_metrics.dt) AS date
+           FROM {{ source("public", "albums_metrics") }}
+        )
+ SELECT DISTINCT ranked_albums.album_id,
+    ranked_albums.likes_count,
+    ranked_albums.date
+   FROM ranked_albums
+  ORDER BY ranked_albums.date
