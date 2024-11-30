b3cba696d801988d7667359b510f1df47bf635d3
evgenij dmitriev <evgenij@3536425-tk81101.twc1.net>
2024-11-30 13:31:21 +0300

    изменение представления popular_tracks_of_artists

--- models/my_sql_models/popular_tracks_of_artists.sql
+++ models/my_sql_models/popular_tracks_of_artists.sql
@@ -1,7 +1,9 @@
 select artist_id,
        a.name,
+       al.title as album_title,
        popular_track_id,
-       title,
-       replace(popular_tracks.cover_uri, '%%'::text, '400x400'::text) AS cover_uri
-from {{ source("public", "popular_tracks") }}
-  left join {{ source("public", "artists") }} as a using (artist_id)       
+       pt.title,
+       replace(pt.cover_uri, '%%'::text, '400x400'::text) AS cover_uri
+from {{ source("public", "popular_tracks") }} as pt
+  left join {{ source("public", "artists") }} as a using (artist_id)
+  left join {{ source("public", "albums") }} as al using (artist_id)       
