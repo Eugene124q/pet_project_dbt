438a4c39703c230a1ad74fe74b3854b54d4cb3e0
evgenij dmitriev <evgenij@3536425-tk81101.twc1.net>
2024-12-04 10:36:02 +0300

    изменение представления top_5_albums_per_artist_view

--- models/my_sql_models/top_5_albums_per_artist_view.sql
+++ models/my_sql_models/top_5_albums_per_artist_view.sql
@@ -1,32 +1,33 @@
-WITH latestlikes AS (
-         SELECT roav.album_id,
-            roav.title,
-            roav.likes_count,
-            roav.artist_id,
-            roav.name,
-            max(roav.date) AS latest_date
-           FROM {{ ref("ratings_of_albums_view") }} as roav
-          GROUP BY roav.album_id, roav.title, roav.likes_count, roav.artist_id, roav.name
-        ), sum_likes_count AS (
-         SELECT latestlikes.artist_id,
-            latestlikes.name,
-            latestlikes.album_id,
-            latestlikes.title,
-            sum(latestlikes.likes_count) AS total_likes
-           FROM latestlikes
-          GROUP BY latestlikes.artist_id, latestlikes.name, latestlikes.album_id, latestlikes.title
-          ORDER BY (sum(latestlikes.likes_count)) DESC
-        ), ranked_albums AS (
-         SELECT sum_likes_count.artist_id,
-            sum_likes_count.name,
-            sum_likes_count.album_id,
-            sum_likes_count.title,
-            sum_likes_count.total_likes,
-            row_number() OVER (PARTITION BY sum_likes_count.artist_id ORDER BY sum_likes_count.total_likes DESC) AS rank
-           FROM sum_likes_count
-        )
- SELECT ranked_albums.name,
-    ranked_albums.title,
-    ranked_albums.total_likes
-   FROM ranked_albums
-  WHERE ranked_albums.rank <= 5
+with total_likes as 
+(
+	SELECT album_id,
+	       title,
+	       genres,
+	       sum(likes_count) as total_likes,
+	       artist_id,
+	       name,
+	       date
+	FROM {{ ref("ratings_of_albums_view") }} 
+	GROUP BY album_id, title, genres, artist_id, name, date
+	order by total_likes desc
+),
+ranked_albums as 
+(
+	select album_id,
+		   title,
+		   genres,
+		   total_likes,
+		   artist_id,
+		   name,
+		   date,
+		   row_number() OVER (PARTITION BY artist_id ORDER BY total_likes DESC) AS rank
+	from total_likes
+)
+select name,
+       artist_id,
+       album_id,
+	    title,
+	    genres,
+	    total_likes
+from ranked_albums
+where rank <= 5
