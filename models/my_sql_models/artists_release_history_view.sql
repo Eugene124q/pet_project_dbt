9ff5e4fd48064975c5a40d0dbe0feb15fb597bd7
evgenij dmitriev <evgenij@3536425-tk81101.twc1.net>
2024-12-04 10:13:38 +0300

    изменение представления artists_releases_history_view

--- models/my_sql_models/artists_release_history_view.sql
+++ models/my_sql_models/artists_release_history_view.sql
@@ -1,10 +1,18 @@
-SELECT artist_id,
-       name,
-       countries,
-       album_id,
-       title,
-       genres,
-       release_date,
-       label_id,
-       label_name
-   FROM {{ ref("ratings_of_albums_view") }} 
+ with generated_dates as 
+ (
+   SELECT generate_series((( SELECT min(release_date) AS min
+                              FROM {{ ref("ratings_of_albums_view") }} as roav))::timestamp without time zone, CURRENT_DATE - '1 day'::interval, '1 day'::interval)::date AS date
+ )
+ select gd.date,
+        CASE
+            WHEN gd.date = roav.release_date THEN roav.release_date
+            ELSE NULL::date
+        END AS release_date,
+        roav.album_id,
+        roav.title,
+        roav.artist_id,
+        roav.name,
+        roav.likes_count
+   FROM generated_dates as gd
+     LEFT JOIN {{ ref("ratings_of_albums_view") }} as roav ON gd.date = roav.release_date
+  ORDER BY gd.date asc
